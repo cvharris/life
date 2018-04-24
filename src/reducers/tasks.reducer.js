70e42f7a72830ff990f20a5f3f4176a6b4f61298
harrisch <charlie.harris@wwt.com>
2018-04-24 09:11:33 -0500

    Normalizes tasks reducer and state, adds tests

---
+++ src/reducers/tasks.reducer.js
@@ -0,0 +1,108 @@
+import update from 'immutability-helper'
+import {
+  ADD_TODO,
+  UPDATE_TODO,
+  DELETE_TODO,
+  MOVE_TODOS,
+  UPDATE_TODO_POSITIONS,
+  FILTER_TODOS
+} from '../conf/ActionTypes'
+import todo from './todo.reducer'
+
+export const updateTodo = todo => ({ type: UPDATE_TODO, payload: todo })
+export const addTodo = todo => ({ type: ADD_TODO, payload: todo })
+export const deleteTodo = todo => ({ type: DELETE_TODO, payload: todo })
+export const moveTodos = (dragIndex, hoverIndex) => ({
+  type: MOVE_TODOS,
+  payload: { dragIndex, hoverIndex }
+})
+export const updateTodoPositions = () => ({ type: UPDATE_TODO_POSITIONS })
+export const filterTodos = (categoryId, areaId) => ({
+  type: FILTER_TODOS,
+  payload: { categoryId, areaId }
+})
+
+// Initial State
+export const initialState = {
+  allIds: [],
+  byId: {}
+}
+
+export default (state = initialState, { type = '', payload }) => {
+  switch (type) {
+    case ADD_TODO:
+      payload.position = state.allIds.length
+      return {
+        allIds: [...state.allIds, payload.id],
+        byId: {
+          ...state.byId,
+          [payload.id]: todo(undefined, { type, payload })
+        }
+      }
+    case UPDATE_TODO:
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          [payload.id]: todo(state.byId[payload.id], { type, payload })
+        }
+      }
+    case DELETE_TODO:
+      const { [payload.id]: deletedTodo, ...newState } = state.byId
+      return {
+        byId: newState,
+        allIds: state.allIds.filter(tId => tId !== payload.id)
+      }
+    case MOVE_TODOS:
+      const { dragId, hoverId } = payload
+      const dragIndex = state.byId[dragId].position
+      const hoverIndex = state.byId[hoverId].position
+      const newIdsList = update(state.allIds, {
+        $splice: [[dragIndex, 1], [hoverIndex, 0, dragId]]
+      })
+      const newDragPosition = newIdsList.indexOf(dragId)
+      const newHoverPosition = newIdsList.indexOf(hoverId)
+
+      return {
+        allIds: newIdsList,
+        byId: {
+          ...state.byId,
+          [dragId]: { ...state.byId[dragId], position: newDragPosition },
+          [hoverId]: { ...state.byId[hoverId], position: newHoverPosition }
+        }
+      }
+    case 'MAP_TASKS':
+      return {
+        allIds: payload.map(t => t.id),
+        byId: payload.reduce((map, t) => {
+          map[t.id] = t
+          return map
+        }, {})
+      }
+    // TODO: must transform a list of todos by category/area
+    // case DELETE_CATEGORY:
+    //   return {
+    //     ...state,
+    //     todos: state.todos.map(t => todo(t, { type, payload }))
+    //   }
+    // TODO: move this action to different reducer
+    // case FILTER_TODOS:
+    //   return {
+    //     ...state,
+    //     filteredTodos: state.todos.filter(t => {
+    //       if (!payload.categoryId && !payload.areaId) {
+    //         return true
+    //       } else if (!payload.areaId) {
+    //         return t.area.category.id === payload.categoryId
+    //       } else {
+    //         return (
+    //           t.area.id === payload.areaId &&
+    //           t.area.category.id === payload.categoryId
+    //         )
+    //       }
+    //     })
+    //   }
+    default:
+      return state
+  }
+}
---
+++ src/reducers/tasks.reducers.spec.js
@@ -0,0 +1,240 @@
+import tasks, { initialState } from './tasks.reducer'
+import * as types from '../conf/ActionTypes'
+const oldData = require('../../data.json')
+
+describe('tasks', () => {
+  it('should return the initial state when no state is given', () => {
+    expect(tasks(undefined, { payload: undefined })).toEqual(initialState)
+  })
+
+  it('should add tasks', () => {
+    const newTask = { id: '5678', description: 'derp' }
+    const newState = tasks(initialState, {
+      type: types.ADD_TODO,
+      payload: newTask
+    })
+
+    expect(newState.allIds).toContain(newTask.id)
+    expect(newState.byId).toHaveProperty(newTask.id)
+  })
+
+  it('should update an individual task', () => {
+    const basicState = {
+      allIds: ['5678'],
+      byId: { '5678': { id: '5678', description: 'derp' } }
+    }
+    const newState = tasks(basicState, {
+      type: types.UPDATE_TODO,
+      payload: { id: '5678', description: 'yarp' }
+    })
+    expect(newState.byId['5678'].description).toBe('yarp')
+    expect(newState.allIds).toEqual(basicState.allIds)
+  })
+
+  it('should delete tasks, with extreme prejudice', () => {
+    const basicState = {
+      allIds: ['5678'],
+      byId: { '5678': { id: '5678', description: 'derp' } }
+    }
+    const newState = tasks(basicState, {
+      type: types.DELETE_TODO,
+      payload: { id: '5678' }
+    })
+    expect(newState.allIds).not.toHaveProperty('5678')
+    expect(newState.allIds.length).toBe(0)
+  })
+
+  describe('moving tasks', () => {
+    let basicState
+
+    beforeEach(() => {
+      basicState = {
+        allIds: [1, 2, 3, 4, 5],
+        byId: {
+          1: { id: 1, position: 0 },
+          2: { id: 2, position: 1 },
+          3: { id: 3, position: 2 },
+          4: { id: 4, position: 3 },
+          5: { id: 5, position: 4 }
+        }
+      }
+    })
+
+    test('moving from middle up to near sibling middle', () => {
+      const dragId = 3
+      const hoverId = 2
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([1, 3, 2, 4, 5])
+      expect(newState.byId[dragId].position).toBe(1)
+      expect(newState.byId[hoverId].position).toBe(2)
+    })
+
+    test('moving from middle down to near sibling middle', () => {
+      const dragId = 2
+      const hoverId = 3
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([1, 3, 2, 4, 5])
+      expect(newState.byId[dragId].position).toBe(2)
+      expect(newState.byId[hoverId].position).toBe(1)
+    })
+
+    test('moving from middle up to far middle', () => {
+      const dragId = 4
+      const hoverId = 2
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([1, 4, 2, 3, 5])
+      expect(newState.byId[dragId].position).toBe(1)
+      expect(newState.byId[hoverId].position).toBe(2)
+    })
+
+    test('moving from middle down to far middle', () => {
+      const dragId = 2
+      const hoverId = 4
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([1, 3, 4, 2, 5])
+      expect(newState.byId[dragId].position).toBe(3)
+      expect(newState.byId[hoverId].position).toBe(2)
+    })
+
+    test('moving from top to middle', () => {
+      const dragId = 1
+      const hoverId = 3
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([2, 3, 1, 4, 5])
+      expect(newState.byId[dragId].position).toBe(2)
+      expect(newState.byId[hoverId].position).toBe(1)
+    })
+
+    test('moving from bottom up middle', () => {
+      const dragId = 5
+      const hoverId = 3
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([1, 2, 5, 3, 4])
+      expect(newState.byId[dragId].position).toBe(2)
+      expect(newState.byId[hoverId].position).toBe(3)
+    })
+
+    test('moving from bottom up middle', () => {
+      const dragId = 5
+      const hoverId = 3
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([1, 2, 5, 3, 4])
+      expect(newState.byId[dragId].position).toBe(2)
+      expect(newState.byId[hoverId].position).toBe(3)
+    })
+
+    test('moving from bottom up middle', () => {
+      const dragId = 5
+      const hoverId = 3
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([1, 2, 5, 3, 4])
+      expect(newState.byId[dragId].position).toBe(2)
+      expect(newState.byId[hoverId].position).toBe(3)
+    })
+
+    test('moving from middle up to top', () => {
+      const dragId = 3
+      const hoverId = 1
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([3, 1, 2, 4, 5])
+      expect(newState.byId[dragId].position).toBe(0)
+      expect(newState.byId[hoverId].position).toBe(1)
+    })
+
+    test('moving from middle down to bottom', () => {
+      const dragId = 3
+      const hoverId = 5
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([1, 2, 4, 5, 3])
+      expect(newState.byId[dragId].position).toBe(4)
+      expect(newState.byId[hoverId].position).toBe(3)
+    })
+
+    test('moving from top to bottom', () => {
+      const dragId = 1
+      const hoverId = 5
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([2, 3, 4, 5, 1])
+      expect(newState.byId[dragId].position).toBe(4)
+      expect(newState.byId[hoverId].position).toBe(3)
+    })
+
+    test('moving from bottom to top', () => {
+      const dragId = 5
+      const hoverId = 1
+
+      const newState = tasks(basicState, {
+        type: types.MOVE_TODOS,
+        payload: { dragId, hoverId }
+      })
+
+      expect(newState.allIds).toMatchObject([5, 1, 2, 3, 4])
+      expect(newState.byId[dragId].position).toBe(0)
+      expect(newState.byId[hoverId].position).toBe(1)
+    })
+  })
+
+  it('converts old data to new', () => {
+    console.log(
+      tasks(initialState, {
+        type: 'MAP_TASKS',
+        payload: oldData.todoList.todos
+      })
+    )
+  })
+})
